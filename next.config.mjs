ad11bf7ddae00d8fdc10fb9db4e675da834f5d59
mrOmsoni <arjunsoni7470@gmail.com>
2026-04-16 18:19:38 +0530

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images:{
-    remotePatterns:[
-      {
-        protocol:"https",
-        hostname:"randomuser.me",
-      },
-    ],
-  },
-  experimental: {
-    turbo: true, // ✅ explicitly enable Turbopack
-    serverComponentsExternalPackages: ['@vercel/turbopack-next'], // helps with internal font resolution
-  },
-  compiler: {
-    // Optional: enable styled-components or other compiler tweaks if needed
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+  serverExternalPackages: ["@prisma/client"],
 };
 
 export default nextConfig;
